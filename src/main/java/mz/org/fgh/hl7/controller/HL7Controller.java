d330ab3b785787e5e50c55d5bf4d7db36397eca4
Yassin Nurmahomed <yassin.nurmahomed@fgh.org.mz>
2023-02-23 10:25:54 +0200

    Implement alert mechanism and display success message

--- src/main/java/mz/org/fgh/hl7/controller/HL7Controller.java
+++ src/main/java/mz/org/fgh/hl7/controller/HL7Controller.java
@@ -7,11 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.validation.Valid;
+import mz.org.fgh.hl7.Alert;
 import mz.org.fgh.hl7.Hl7FileForm;
 import mz.org.fgh.hl7.Location;
 import mz.org.fgh.hl7.service.LocationService;
@@ -27,31 +28,22 @@ public class HL7Controller {
     }
 
     @GetMapping
-    public String showHl7Form() {
-
-        // @SuppressWarnings("unchecked")
-        // List<Location> allProvinces = (List<Location>) model.getAttribute("allProvinces");
-        // Location province = hl7FileForm.getProvince();
-        // if (province == null) {
-        //     province = allProvinces.get(0);
-        //     hl7FileForm.setProvince(province);
-        // }
-
-        // if (hl7FileForm.getDistrict() == null) {
-        //     hl7FileForm.setDistrict(province.getChildLocations().get(0));
-        // }
-
-        // List<Location> healthFacilities = hl7FileForm.getDistrict().getChildLocations();
-        // model.addAttribute("partitionedHF", ListUtils.partition(healthFacilities, 5));
-
+    public String showHl7Form(Hl7FileForm hl7FileForm, Model model) {
+        setAllProvinces(hl7FileForm, model);
         return "hl7";
     }
 
     @PostMapping
-    public String generateHl7Files(@Valid Hl7FileForm hl7FileForm, BindingResult result, Model model) {
+    public String generateHl7Files(
+        @Valid Hl7FileForm hl7FileForm,
+        BindingResult result,
+        Model model,
+        RedirectAttributes redirectAttrs) {
         if (result.hasErrors()) {
-            return "hl7";
+            return showHl7Form(hl7FileForm, model);
         }
+
+        redirectAttrs.addFlashAttribute(Alert.success("hl7.schedule.success"));
         return "redirect:/";
     }
 
