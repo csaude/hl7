16c4f68b975c70c651ae005d31695bf12a97424c
Yassin Nurmahomed <yassin.nurmahomed@fgh.org.mz>
2023-02-23 11:24:44 +0200

    Add error pages and inject app version
    Error pages for 404 and 500
    
    Use @ControllerAdvice to add appVersion property to models across
    controllers

--- src/main/java/mz/org/fgh/hl7/controller/HelpController.java
+++ src/main/java/mz/org/fgh/hl7/controller/HelpController.java
@@ -1,6 +1,5 @@
 package mz.org.fgh.hl7.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/help")
 public class HelpController {
 
-    @Value("${app.version}")
-    private String appVersion;
-
     @GetMapping
     public String showHelpPage(Model model) {
-        model.addAttribute("appVersion", appVersion);
         return "help";
     }
 }
