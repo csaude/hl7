ab46c4d44b2c667bd6071506ca2d989d0ff7bef1
Yassin Nurmahomed <yassin.nurmahomed@fgh.org.mz>
2023-02-24 10:19:08 +0200

    Add email to form

--- src/main/java/mz/org/fgh/hl7/Hl7FileForm.java
+++ src/main/java/mz/org/fgh/hl7/Hl7FileForm.java
@@ -2,12 +2,17 @@ package mz.org.fgh.hl7;
 
 import java.util.List;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Hl7FileForm {
+    @NotBlank(message = "{hl7.validation.fileform.email.NotBlank.message}")
+    @Email(message = "{hl7.validation.fileform.email.Email.message}")
+    private String email;
     private Location province;
     private Location district;
 
