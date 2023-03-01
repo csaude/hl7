e96109e55a61f4872fc74f556fb83b0a3a5f1d61
Yassin Nurmahomed <yassin.nurmahomed@fgh.org.mz>
2023-03-01 11:48:30 +0200

    Add filename field

--- src/main/java/mz/org/fgh/hl7/Hl7FileForm.java
+++ src/main/java/mz/org/fgh/hl7/Hl7FileForm.java
@@ -5,6 +5,7 @@ import java.util.List;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -13,6 +14,11 @@ public class Hl7FileForm {
     @NotBlank(message = "{hl7.validation.fileform.email.NotBlank.message}")
     @Email(message = "{hl7.validation.fileform.email.Email.message}")
     private String email;
+
+    @NotBlank(message = "{hl7.validation.fileform.filename.NotBlank.message}")
+    @Pattern(regexp= "^[a-zA-Z0-9 _]*$", message="{hl7.validation.fileform.filename.Pattern.message}")
+    private String filename;
+
     private Location province;
     private Location district;
 
