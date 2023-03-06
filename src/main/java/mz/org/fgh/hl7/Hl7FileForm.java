024ee7a8dd21c0bea1fb7bc67884604931ee63b2
Helio Machabane <helio.machabane@gmail.com>
2023-03-06 12:33:52 +0200

    Use Paths.get to concatenate folder location with filename

--- src/main/java/mz/org/fgh/hl7/Hl7FileForm.java
+++ src/main/java/mz/org/fgh/hl7/Hl7FileForm.java
@@ -8,13 +8,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-import java.util.List;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
