6cd9e6f91b704f85de1e4518a854efa4d7e683db
Yassin Nurmahomed <yassin.nurmahomed@fgh.org.mz>
2023-05-17 14:30:32 +0200

    DOI-176 and DOI-177
    * DOI-176 Remove file name field
    * DOI-177 Generate single HL7 file overwriting the previous
    * Use CompletableFuture to handle async computations
    * Rename routes

--- src/main/java/mz/org/fgh/hl7/service/Hl7Service.java
+++ src/main/java/mz/org/fgh/hl7/service/Hl7Service.java
@@ -1,14 +1,41 @@
 package mz.org.fgh.hl7.service;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import ca.uhn.hl7v2.HL7Exception;
 import mz.org.fgh.hl7.model.HL7File;
 import mz.org.fgh.hl7.model.Location;
+import mz.org.fgh.hl7.model.PatientDemographic;
 
 public interface Hl7Service {
-	public void validateCreate(String filename);
-	public void create(String fileName, List<Location> locations) throws HL7Exception;
-    public void delete(String filename);
-    public List<HL7File> findAll();
+    /**
+     * @param locations The locations to be used to generate the HL7 file.
+     * @return The generated HL7 file.
+     * @throws HL7Exception
+     */
+    public CompletableFuture<HL7File> generateHl7File(List<Location> locations) throws HL7Exception;
+
+    /**
+     * @return The generated HL7 file, could be processing.
+     */
+    public CompletableFuture<HL7File> getHl7File();
+
+    /**
+     * @return Previously HL7 file that was succesfully generated or null if none.
+     */
+    public HL7File getPreviousHl7File();
+
+    /**
+     * @return True if the search is available, false otherwise. Due to asynchronous
+     *         nature of the file generation, clients should call this method
+     *         before calling search.
+     */
+    public boolean isSearchAvailable();
+
+    /**
+     * @param partialNID The partial NID to be used to search for patients.
+     * @return The list of patients matching the partial NID.
+     */
+    public List<PatientDemographic> search(String partialNID);
 }
