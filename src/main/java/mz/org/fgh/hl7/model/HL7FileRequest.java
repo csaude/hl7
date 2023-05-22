72ff5eb32bccefdc43c551954fd111caa9991840
Yassin Nurmahomed <yassin.nurmahomed@fgh.org.mz>
2023-05-22 16:37:06 +0200

    DOI-179 Show HFs on partial NID search interface
     * Serialize HL7File at end of generation and load at startup
     * Extract ConfigController and view from HL7Controller
     * Pre-select HF from successfully generated HL7
     * Improve app navigation

---
+++ src/main/java/mz/org/fgh/hl7/model/HL7FileRequest.java
@@ -0,0 +1,18 @@
+package mz.org.fgh.hl7.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class HL7FileRequest implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Location province;
+
+    private Location district;
+
+    private List<Location> healthFacilities;
+}
