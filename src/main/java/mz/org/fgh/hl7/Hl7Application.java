f9ab1ecf79bbea5491909b017ffda2703991daf4
Yassin Nurmahomed <yassin.nurmahomed@fgh.org.mz>
2023-05-23 15:15:36 +0200

    DOI-172 Implement war packaging for HL7 App

--- src/main/java/mz/org/fgh/hl7/Hl7Application.java
+++ src/main/java/mz/org/fgh/hl7/Hl7Application.java
@@ -2,9 +2,16 @@ package mz.org.fgh.hl7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Hl7Application {
+public class Hl7Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Hl7Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Hl7Application.class, args);
