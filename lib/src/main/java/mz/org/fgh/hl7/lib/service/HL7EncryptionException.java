070b306adddafe97d058d3f2446259f3c0e97265
Helio Machabane <helio.machabane@gmail.com>
2024-03-25 11:59:20 +0200

    - Fetch errors from ProcessBuilder when openssl encryption fails.

--- lib/src/main/java/mz/org/fgh/hl7/lib/service/HL7EncryptionException.java
+++ lib/src/main/java/mz/org/fgh/hl7/lib/service/HL7EncryptionException.java
@@ -1,6 +1,10 @@
 package mz.org.fgh.hl7.lib.service;
 
 public class HL7EncryptionException extends RuntimeException {
+	
+	public HL7EncryptionException() {
+	}
+	
     public HL7EncryptionException(Throwable cause) {
         super(cause);
     }
